c512b1872b1fc1785bdadb9355df42e79ec3bae4
Prashant Sharma <sharma.pras06@gmail.com>
2026-01-15 19:48:56 +0800

    Update

--- environment/openenv_maintenance_env.py
+++ environment/openenv_maintenance_env.py
@@ -1,13 +1,24 @@
-
-from openenv import OpenEnv
+import gymnasium as gym
+from gymnasium import spaces
 import random
 
-class MaintenancePlannerEnv(OpenEnv):
+class MaintenancePlannerEnv(gym.Env):
+    metadata = {"render_modes": []}
+
     def __init__(self):
         super().__init__()
+
+        self.action_space = spaces.Discrete(2)  # 0=PM, 1=Delay
+        self.observation_space = spaces.Dict({
+            "day": spaces.Discrete(100),
+            "asset_health": spaces.Box(low=0, high=100, shape=(1,), dtype=int),
+            "cost": spaces.Box(low=0, high=10000, shape=(1,), dtype=int),
+        })
+
         self.max_steps = 50
 
-    def reset(self, seed=None):
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
         self.day = 0
         self.asset_health = 100
         self.cost = 0
@@ -17,11 +28,11 @@ class MaintenancePlannerEnv(OpenEnv):
         self.day += 1
         reward = 0
 
-        if action == 0:
+        if action == 0:  # Preventive Maintenance
             self.asset_health += 5
             self.cost += 100
             reward += 3
-        elif action == 1:
+        else:  # Delay
             self.asset_health -= random.randint(5, 15)
             reward -= 2
 
@@ -30,11 +41,13 @@ class MaintenancePlannerEnv(OpenEnv):
             self.asset_health = 50
 
         terminated = self.day >= self.max_steps
-        return self._obs(), reward, terminated, False, {}
+        truncated = False
+
+        return self._obs(), reward, terminated, truncated, {}
 
     def _obs(self):
         return {
             "day": self.day,
-            "asset_health": self.asset_health,
-            "cost": self.cost
+            "asset_health": [self.asset_health],
+            "cost": [self.cost],
         }
